f9d5dfeab13d6bb7836b3d54d0eb26fd72a01c32
Adrian Lyjak <adrianlyjak@gmail.com>
2025-09-27 13:13:31 -0400

    Update all of the templates to remove test-proj, and migrate from vibe-llama templates

--- test-proj/ui/src/libs/config.ts
+++
@@ -1,3 +0,0 @@
-export const APP_TITLE = "Test Proj";
-export const AGENT_NAME = import.meta.env.VITE_LLAMA_DEPLOY_DEPLOYMENT_NAME;
-export const INDEX_NAME = "document_qa_index";
--- test-proj/ui/src/libs/utils.ts
+++
@@ -1,7 +0,0 @@
-export function toHumanResponseRawEvent(str: string) {
-  return {
-    __is_pydantic: true,
-    value: { _data: { response: str } },
-    qualified_name: "workflows.events.HumanResponseEvent",
-  };
-}
